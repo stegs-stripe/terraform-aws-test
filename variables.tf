0c9d35e042aa1dec67fa2c05700748c4f2bff4bd
bstegemoller <bri.stegemoller@gmail.com>
2020-03-04 13:49:12 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 /* aws credentials */
-variable "aws_access_key<>\><\\'\"" {
-  type = "string"
+variable "aws_access_key" {
+  type = "string\<\>\"\'"
 }
 variable "aws_secret_key" {
   type = "string"
